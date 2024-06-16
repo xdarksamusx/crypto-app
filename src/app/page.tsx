7f641ab353ef47ea24378b0ded5aebc2518415e9
gus1000 <gramirez644@gmail.com>
2024-06-16 10:35:48 -0700

    deleted the home directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import React from "react";
+
 import StoreProvider from "./StoreProvider";
 import { useAppSelector, useAppDispatch } from "@/lib/hooks";
 import { addTodo } from "@/lib/features/todo";
