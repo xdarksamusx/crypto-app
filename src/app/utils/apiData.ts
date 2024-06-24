e7273a1c7119c44a1cfc18e7a6b12cb58e1c9d56
gus1000 <gramirez644@gmail.com>
2024-06-23 18:06:27 -0700

    completed slice that fetches coin data and updates state. layout has a provider that wraps every component/page

--- src/app/utils/apiData.ts
+++ src/app/utils/apiData.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
 
 export const fetchCoins = async () => {
-  const response = await axios(
-    `https://api.coingecko.com/api/v3/coins/markets`
+  const response = await axios.get(
+    `https://api.coingecko.com/api/v3/coins/markets`,
+    {
+      params: {
+        vs_currency: "usd",
+        order: "market_cap_desc",
+        per_page: 20,
+        page: 1,
+        sparkline: false,
+      },
+    }
   );
 
   return response.data;
