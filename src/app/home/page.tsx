5ec987d518da6a5d006166119249440310e28a45
gus1000 <gramirez644@gmail.com>
2024-07-04 14:28:25 -0700

    recreated storage object so that it includes 360 data points

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import React, { useEffect, useState, useRef } from "react";
-import { useAppSelector, useAppDispatch } from "../redux/hooks";
-import { fetchTop20Coins } from "../redux/features/marketSlice";
-import Table from "../components/Table";
+import { useAppSelector, useAppDispatch } from "../../redux/hooks";
+import { fetchTop20Coins } from "../../redux/features/marketSlice";
+import Table from "../../components/Table";
 
-import Header from "../components/Header";
+import Header from "../../components/Header";
 
-import Navigation from "../components/Navigation";
+import Navigation from "../../components/Navigation";
 
 const Home: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -16,7 +16,9 @@ const Home: React.FC = () => {
   const error = useAppSelector((state) => state.coins.error);
   const fetchOnce = useRef(false);
 
+  // 168 data points per 7 days
   useEffect(() => {
+    console.log(coins);
     if (!fetchOnce.current) {
       if (status === "idle" && coins.length === 0) {
         dispatch(fetchTop20Coins());
--- src/app/utils/dateFunctions.ts
+++ src/app/utils/dateFunctions.ts
@@ -6,7 +6,7 @@ export const currentDate = (): number => {
 
 export const previousDate = (): number => {
   const currentDate = new Date();
-  currentDate.setDate(currentDate.getDate() - 7);
+  currentDate.setDate(currentDate.getDate() - 360);
   const unixTime = Math.floor(currentDate.getTime() / 1000);
   return unixTime;
 };
