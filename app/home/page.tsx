84402224280ee2c9c8265cc33f3de585f8dbd9a2
gus1000 <gramirez644@gmail.com>
2024-07-08 09:00:04 -0700

    resolved tailwind config issues. Content array needed to be filled in config.js file

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,21 +1,21 @@
 "use client";
+import "../globals.css";
 
 import React, { useEffect, useState, useRef } from "react";
-import { useAppSelector, useAppDispatch } from "@/redux/hooks";
+import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 import { fetchTop20Coins } from "../../redux/features/marketSlice";
 import Table from "../../components/Table";
 
 import Header from "../../components/Header";
 
 import Navigation from "../../components/Navigation";
-const Home: React.FC = () => {
+const Home = () => {
   const dispatch = useAppDispatch();
   const coins = useAppSelector((state) => state.coins.coins);
   const status = useAppSelector((state) => state.coins.status);
   const error = useAppSelector((state) => state.coins.error);
   const fetchOnce = useRef(false);
 
-  // 168 data points per 7 days
   useEffect(() => {
     if (!fetchOnce.current) {
       if (status === "idle" && coins.length === 0) {
