4b7fac0c51e8ba3c678593c1e01b74e31e9bacb2
gus1000 <gramirez644@gmail.com>
2024-07-16 13:34:16 -0700

    added color coding for percent changes

--- redux/features/sortSlice.ts
+++ redux/features/sortSlice.ts
@@ -1,27 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { getInitialCoinState } from "../../utils/apiData";
-import { CoinState } from "../../utils/interfaces";
-// import {
-//   sortMarketCapAscending,
-//   sortMarketCapDescending,
-//   sortCurrentPriceAscending,
-//   sortHourlyPriceChangeDescending,
-//   sortHourlyPriceChangeAscending,
-//   sortCurrentPriceDescending,
-//   sortCurrentWeeklyPriceChangeAscending,
-//   sortCurrentWeeklyPriceChangeDescending,
-//   sortRankAscending,
-//   sortRankDescending,
-//   sortNameAscending,
-//   sortNameDescending,
-//   sortDailyPriceChangeAscending,
-//   sortDailyPriceChangeDescending,
-//   sortTotalVolumeAscending,
-//   sortTotalVolumeDescending,
-// } from "../utils/sortFunctions";
+import { CoinState, CoinData } from "../../utils/interfaces";
+
 const initialState = {
-  coins: getInitialCoinState(),
+  coins: getInitialCoinState().map((coin) => ({
+    ...coin,
+    price_change_percentage_24h:
+      coin.market_cap_change_percentage_24h > 0 ? "green" : "red",
+    weeklyColor: coin.weeklyPriceChange > 0 ? "green" : "red",
+    hourlyColor: coin.hourly_price_change > 0 ? "green" : "red",
+  })),
   sortKey: "market_cap",
 };
 
@@ -57,9 +46,24 @@ const sortSlice = createSlice({
     setCoins(state, action) {
       state.coins = action.payload;
     },
+
+    updateColors(state) {
+      state.coins.forEach((coin) => {
+        coin.hourlyColor =
+          Number(coin.hourly_price_change) > 0 ? "green" : "red";
+        coin.dailyColor =
+          Number(coin.market_cap_change_percentage_24h) > 0 ? "green" : "red";
+        coin.weeklyColor = Number(coin.weeklyPriceChange) > 0 ? "green" : "red";
+      });
+    },
   },
 });
 
-export const { sortByIncreasing, sortByDecreasing, setSortKey, setCoins } =
-  sortSlice.actions;
+export const {
+  sortByIncreasing,
+  sortByDecreasing,
+  setSortKey,
+  setCoins,
+  updateColors,
+} = sortSlice.actions;
 export default sortSlice.reducer;
