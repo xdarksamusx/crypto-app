0c7abdaa69cd23fde07072c5a3df45ef99b8a2bb
gus1000 <gramirez644@gmail.com>
2024-08-07 16:44:22 -0700

    fetched coin  api for page data

--- app/coins/[coinId]/page.tsx
+++ app/coins/[coinId]/page.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+"use client";
 
-export default function CoinStats({
-  params,
-}: {
-  params: { coinName: string };
-}) {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {params.coinName}
-    </main>
+import { useEffect } from "react";
+import axios from "axios";
+
+const fetchData = async (coinId: string) => {
+  console.log("hi", coinId);
+  const { data } = await axios.get(
+    `https://api.coingecko.com/api/v3/coins/${coinId}`
   );
-}
+  return data;
+};
+
+const Page = ({ params }: { params: { coinId: string } }) => {
+  const { coinId } = params;
+  try {
+    useEffect(() => {
+      const data = fetchData(coinId);
+      console.log(data);
+    }, [params]);
+
+    return <div>me hi</div>;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export default Page;
