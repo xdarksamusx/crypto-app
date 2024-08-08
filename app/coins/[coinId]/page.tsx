2122531917e331be3f021178a4cbae87c912b4ab
gus1000 <gramirez644@gmail.com>
2024-08-08 10:00:02 -0700

    called coin api data for coin description

--- app/coins/[coinId]/page.tsx
+++ app/coins/[coinId]/page.tsx
@@ -1,28 +1,55 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const fetchData = async (coinId: string) => {
-  console.log("hi", coinId);
-  const { data } = await axios.get(
-    `https://api.coingecko.com/api/v3/coins/${coinId}`
-  );
-  return data;
+  try {
+    console.log("hi", coinId);
+    const { data } = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${coinId}`
+    );
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
 };
 
 const Page = ({ params }: { params: { coinId: string } }) => {
   const { coinId } = params;
-  try {
-    useEffect(() => {
-      const data = fetchData(coinId);
-      console.log(data);
-    }, [params]);
 
-    return <div>me hi</div>;
-  } catch (error) {
-    console.log("error", error);
-  }
+  const [pageData, setPageData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const getData = async () => {
+      try {
+        const result = await fetchData(coinId);
+        if (isMounted) {
+          setPageData(result);
+          console.log("results", result);
+        }
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    getData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [coinId]);
+
+  return (
+    <>
+      <div>me hi</div>
+
+      {pageData && <div>{pageData.description?.en}</div>}
+    </>
+  );
 };
 
 export default Page;
