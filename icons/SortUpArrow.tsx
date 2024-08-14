c0885d4e6aedeacfe5a1d822b2b5a017aab0627f
gus1000 <gramirez644@gmail.com>
2024-08-14 09:31:08 -0700

    added color filling to navigation SVG's

--- icons/SortUpArrow.tsx
+++ icons/SortUpArrow.tsx
@@ -12,7 +12,6 @@ const SortUpArrow: React.FC = () => {
   return (
     <>
       <svg
-        // style={{ fill: "var(--svg-color)" }}
         className="h-2 w-2 bg-svg-color  my-4 relative  "
         height="10"
         width="7"
